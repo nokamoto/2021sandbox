ee82d18e05fdab7385272570a7efbac05f668477
Naofumi Okamoto <nokamoto.engr@gmail.com>
2021-09-18 13:19:56 +0000

    Read delete-gcr input values from core

--- .github/actions/delete-gcr/src/main.ts
+++ .github/actions/delete-gcr/src/main.ts
@@ -1,5 +1,20 @@
+import * as core from "@actions/core";
+
 async function run(): Promise<void> {
-  console.log("todo");
+  try {
+    const username = core.getInput("username");
+    const packagename = core.getInput("packagename");
+    const token = core.getInput("token");
+    const dryrun = core.getBooleanInput("dryrun");
+    console.log("username=", username, "packagename=", packagename, "token=", token, "dryrun=", dryrun);
+  } catch (err) {
+    if (err instanceof Error) {
+      core.setFailed(err.message);
+    } else {
+      console.log(err);
+      core.setFailed("unknown error");
+    }
+  }
 }
 
 run();
