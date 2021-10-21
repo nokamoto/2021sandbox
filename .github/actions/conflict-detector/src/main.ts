dd28d4543384a609caa6c41bca8ba904f1dbb467
Naofumi Okamoto <nokamoto.engr@gmail.com>
2021-10-21 14:12:40 +0900

    Implement conflict detector (#66)

--- .github/actions/conflict-detector/src/main.ts
+++ .github/actions/conflict-detector/src/main.ts
@@ -1,8 +1,54 @@
 import * as core from "@actions/core";
+import { createComment, getPull, getPulls } from "./pr";
 
 async function run(): Promise<void> {
   try {
-    console.log("todo");
+    const token = core.getInput("token");
+    const owner = core.getInput("owner");
+    const repo = core.getInput("repo");
+    const body = core.getInput("body");
+    const dryrun = core.getBooleanInput("dryrun");
+
+    console.log(
+      "token =",
+      token,
+      "owner =",
+      owner,
+      "repo =",
+      repo,
+      "body =",
+      body,
+      "dryrun =",
+      dryrun
+    );
+
+    const pulls = await getPulls(token, owner, repo);
+
+    const conflictingPulls: number[] = [];
+    for (let i = 0; i < pulls.length; i++) {
+      let pull = pulls[i];
+      pull = await getPull(token, owner, repo, pull);
+      console.log("pull =", pull);
+      switch (pull.state) {
+        case "dirty":
+          conflictingPulls.push(pull.number);
+          break;
+        default:
+          console.log("skip state =", pull.state);
+          break;
+      }
+    }
+
+    console.log("conflicting =", conflictingPulls);
+
+    for (let i = 0; i < conflictingPulls.length; i++) {
+      if (dryrun) {
+        console.log("dryrun comment: #", conflictingPulls[i]);
+      } else {
+        console.log("run: #", conflictingPulls[i]);
+        await createComment(token, owner, repo, conflictingPulls[i], body);
+      }
+    }
   } catch (err) {
     if (err instanceof Error) {
       core.setFailed(err.message);
